6d16ceffc20d3f0995b96b8485bb8c59d4b8c894
Chandler Davis <chandler@photonfighterlabs.com>
2018-12-12 21:50:35 -0500

    Added weather api functionality along with minor changes.

--- app/app.js
+++ app/app.js
@@ -6,11 +6,19 @@
 
 const {App} = require('jovo-framework');
 
+const API_CONFIG = require('./config.js');
+const API_KEY = API_CONFIG.API_KEY;
+
+const https = require('https');
+//const Results = require('results');
+
 const config = {
     logging: true,
 };
 
 const app = new App(config);
+
+var temp = 0.0;
 
 
 // =================================================================================
@@ -21,6 +29,21 @@ app.setHandler({
     'LAUNCH': function() {
         // Eventually, this will be this.ask(), but as of right now it will cause errors because this piece of shit doesn't do anything yet
         this.tell("Hey, Helen's Ghost here. What can I do for you?");
+
+        https.get('https://api.openweathermap.org/data/2.5/weather?zip=45056,us&units=imperial&APPID=' + API_KEY, (res) => {
+          let rawData='';
+
+          res.on('data', (chunk) => { rawData += chunk });
+          res.on('end', () => {
+            console.log('Function Response: ' + JSON.parse(rawData).main.temp);
+            temp = Math.round(JSON.parse(rawData).main.temp);
+
+
+            console.log(temp);
+
+      });
+    });
+
     },
 
     'NEW_USER': function() {
@@ -28,7 +51,11 @@ app.setHandler({
     },
 
     'ThermoIntent': function() {
-      this.tell("Looks like blah blah blah")
+      if(temp >= 58) {
+        this.tell("It's currently " + temp + " degrees outside, so my best guess is that your thermostat is set to cool");
+      } else {
+        this.tell("It's currently " + temp + " degrees outside, so my best guess is that your thermostat is set to heat");
+      }
     }
 });
 
